cf82713ceb93e7d925882d74499b08f3b057f917
joao_pena <joao.pena@smarketsolutions.com.br>
2025-04-15 14:56:00 -0300

    Adicionando o 3 function

--- boot.dev/learn-go/ch3-functions/3/main.go
+++ boot.dev/learn-go/ch3-functions/3/main.go
@@ -26,3 +26,16 @@ Convert the prices from dollars to pennies. If the given tier doesn't match any 
 
 
 */
+
+package main
+
+func getMonthlyPrice(tier string) int {
+	if tier == "basic" {
+		return 10000
+	}else if tier == "premium" {
+		return 15000
+	}else if tier == "enterprise" {
+		return 50000
+	}
+	return 0.0
+}
