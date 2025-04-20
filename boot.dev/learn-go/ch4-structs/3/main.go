509c302b91a9d01da04b109bb976213403074aab
joao_pena <joao.pena@smarketsolutions.com.br>
2025-04-20 10:24:03 -0300

    Adicionando exemplo de structs anonimas, usamos quando vamos instancias um objeto apenas para usar uma vez

---
+++ boot.dev/learn-go/ch4-structs/3/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type car struct {
+	brand string
+	model string
+	doors int
+	mileage int
+	// wheel is a field containing an anonymous struct
+	wheel struct {
+	  radius int
+	  material string
+	}
+  }
+
+func main(){
+	myCar := struct {
+		brand string
+		model string
+	  } {
+		brand: "Toyota",
+		model: "Camry",
+	  }
+}
