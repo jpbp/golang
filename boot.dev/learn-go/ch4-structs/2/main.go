bc6dcfd076f9958aa5ae44e77d8763db2bc0b305
joao_pena <joao.pena@smarketsolutions.com.br>
2025-04-20 10:17:39 -0300

    Adicionando exemplo de structs com carro

--- boot.dev/learn-go/ch4-structs/2/main.go
+++ boot.dev/learn-go/ch4-structs/2/main.go
@@ -20,18 +20,18 @@ func formatPrint( oneCar car){
 	fmt.Printf("Meu carro é um %s, modelo %s, ele possui %v portas e sua kilometragem é de %v",oneCar.brand,oneCar.model,oneCar.doors,oneCar.mileage)
 }
 
-// func main1() {
-// 	mywheel := wheel{
-// 		radius: 5,
-// 		material: "rubber",
-// 	}
-// 	myCar := car{
-// 		brand: "onix",
-// 		model: "lt 1.4 aut",
-// 		doors: 4,
-// 		mileage: 135000,
-// 		frontWheel: mywheel,
-// 		backWheel: mywheel,
-// 	}
-// 	formatPrint(myCar)
-// }
+func main1() {
+	mywheel := wheel{
+		radius: 5,
+		material: "rubber",
+	}
+	myCar := car{
+		brand: "onix",
+		model: "lt 1.4 aut",
+		doors: 4,
+		mileage: 135000,
+		frontWheel: mywheel,
+		backWheel: mywheel,
+	}
+	formatPrint(myCar)
+}
