9de239a9277718e2010317f9082ed79e2b3ff956
joao_pena <joao.pena@smarketsolutions.com.br>
2025-04-20 10:26:50 -0300

    Formatando e adicionando a chamada do ao objeto mycar para nao quebrar o codigo

--- boot.dev/learn-go/ch4-structs/3/main.go
+++ boot.dev/learn-go/ch4-structs/3/main.go
@@ -3,23 +3,24 @@ package main
 import "fmt"
 
 type car struct {
-	brand string
-	model string
-	doors int
+	brand   string
+	model   string
+	doors   int
 	mileage int
 	// wheel is a field containing an anonymous struct
 	wheel struct {
-	  radius int
-	  material string
+		radius   int
+		material string
 	}
-  }
+}
 
-func main(){
+func main() {
 	myCar := struct {
 		brand string
 		model string
-	  } {
+	}{
 		brand: "Toyota",
 		model: "Camry",
-	  }
+	}
+	fmt.Println(myCar)
 }
