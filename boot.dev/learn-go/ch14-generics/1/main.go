91c680ef45c640f239a25c9daf9356ddcbbbffe4
jpbp <jppenna04@gmail.com>
2025-05-07 16:09:23 -0300

    Adicionando exercicios sobre generico

---
+++ boot.dev/learn-go/ch14-generics/1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func splitAnySlice[T any](s []T) ([]T, []T) {
+    mid := len(s)/2
+    return s[:mid], s[mid:]
+}
+
+func getLast[T any](s []T) T {
+	var myZero T
+	if len(s) == 0{
+		return myZero
+	}
+	return  s[len(s)-1]
+}
+
+
+func main(){
+	array1 := []int{1,2,3,4,5,6,7,8,9,10}
+	array2 := []string{"jp","ana","marco","mae","pitica"}
+
+	fmt.Println(splitAnySlice(array1))
+	fmt.Println(splitAnySlice(array2))
+	fmt.Println(getLast(array1))
+	fmt.Println(getLast(array2))
+}
+
